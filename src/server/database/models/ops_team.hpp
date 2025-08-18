336156ef764223f6a1d75fc1865120139539d17b
SarahGreyWolf <m.sarahgreywolf@outlook.com>
2025-08-18 16:51:08 +0100

    get assigned ops_team for combat_op from database

--- src/server/database/models/ops_team.hpp
+++ src/server/database/models/ops_team.hpp
@@ -21,7 +21,7 @@ public:
     DEFINE_FIELD(combat_rank_bottom, sqlpp::integer_unsigned);
     DEFINE_FIELD(combat_rank_top, sqlpp::integer_unsigned);
     DEFINE_FIELD(dead_rate, sqlpp::integer_unsigned);
-    DEFINE_FIELD(is_valid, sqlpp::integer_unsigned);
+    DEFINE_FIELD(is_valid, sqlpp::boolean);
     DEFINE_FIELD(mission_id, sqlpp::integer_unsigned);
     DEFINE_FIELD(seed, sqlpp::integer_unsigned);
     DEFINE_FIELD(staff_power, sqlpp::integer_unsigned);
@@ -74,42 +74,43 @@ public:
 
     GET_FIELD_H(std::uint64_t, team_id); // The id of the team (possibly a
                                          // mission_teams table primary_key?)
-    GET_FIELD_H(std::uint64_t, armored); // Number of armored vehicles deployed
-    GET_FIELD_H(std::uint64_t, battle_gear);  // Is battle gear deployed
-    GET_FIELD_H(std::uint64_t, car);          // Number of cars deployed
-    GET_FIELD_H(std::uint64_t, combat_count); // Deployed Main Combat Unit Size
-    GET_FIELD_H(std::uint64_t,
+    GET_FIELD_H(std::uint32_t, armored); // Number of armored vehicles deployed
+    GET_FIELD_H(std::uint32_t, battle_gear);  // Is battle gear deployed
+    GET_FIELD_H(std::uint32_t, car);          // Number of cars deployed
+    GET_FIELD_H(std::uint32_t, combat_count); // Deployed Main Combat Unit Size
+    GET_FIELD_H(std::uint32_t,
                 combat_rank_bottom); // When a range of ranks (like S+ - S) this
                                      // is the lowest rank
-    GET_FIELD_H(std::uint64_t,
+    GET_FIELD_H(std::uint32_t,
                 combat_rank_top); // The highest rank deployed (see Staff Ranks
                                   // (take rank and add 1))
-    GET_FIELD_H(std::uint64_t, dead_rate); // ????
-    GET_FIELD_H(std::uint64_t,
+    GET_FIELD_H(std::uint32_t, dead_rate); // ????
+    GET_FIELD_H(bool,
                 is_valid); // When value is 1, the mission is marked as
                            // currently active for the client
     GET_FIELD_H(std::uint64_t,
                 mission_id); // Matches the op mission_id, leading credence to
                              // database relations, maybe
-    GET_FIELD_H(std::uint64_t, seed); // Seems to be 0 regardless of if it's an
+    GET_FIELD_H(std::uint32_t, seed); // Seems to be 0 regardless of if it's an
                                       // active team or not, and doesn't match
-    GET_FIELD_H(std::uint64_t, staff_power); // Total Fighting Ability for this
+    GET_FIELD_H(std::uint32_t, staff_power); // Total Fighting Ability for this
                                              // team (How it calculates Success
-    GET_FIELD_H(std::uint64_t, sub_count);   // Deployed Supporting Unit Size
-    GET_FIELD_H(std::uint64_t,
+    GET_FIELD_H(std::uint32_t, sub_count);   // Deployed Supporting Unit Size
+    GET_FIELD_H(std::uint32_t,
                 sub_rank_bottom); // When a range of ranks (like S+ - S) this is
                                   // the lowest rank deployed
-    GET_FIELD_H(std::uint64_t,
+    GET_FIELD_H(std::uint32_t,
                 sub_rank_top); // The highest rank deployed (see Staff Ranks
                                // (take rank and add 1))
-    GET_FIELD_H(std::uint64_t, tank);       // Number of tanks deployed
-    GET_FIELD_H(std::uint64_t, team_power); // Seems to match staff_power
-    GET_FIELD_H(std::uint64_t, time);  // Time (in seconds) left of the mission
-    GET_FIELD_H(std::uint64_t, truck); // Number of Trucks deployed
-    GET_FIELD_H(std::uint64_t, walker_gear); // Number of walker gear deployed
-    GET_FIELD_H(std::uint64_t,
+    GET_FIELD_H(std::uint32_t, tank);       // Number of tanks deployed
+    GET_FIELD_H(std::uint32_t, team_power); // Seems to match staff_power
+    GET_FIELD_H(std::uint32_t, time);  // Time (in seconds) left of the mission
+    GET_FIELD_H(std::uint32_t, truck); // Number of Trucks deployed
+    GET_FIELD_H(std::uint32_t, walker_gear); // Number of walker gear deployed
+    GET_FIELD_H(std::uint32_t,
                 win_rate); // Unknown, doesn't match the missions win rate,
                            // maybe is their success level
     GET_FIELD_H(std::uint64_t, player_id);
 };
+std::optional<ops_team> find(std::uint64_t team_id);
 } // namespace database::ops_team
--- src/server/platforms/tppstm/endpoints/main/commands/cmd_get_combat_deploy_list.cpp
+++ src/server/platforms/tppstm/endpoints/main/commands/cmd_get_combat_deploy_list.cpp
@@ -6,6 +6,31 @@
 #include "database/models/ops_team.hpp"
 
 namespace emulator::tpp {
+
+void create_empty_team(nlohmann::json &team) {
+    team["armored"] = 0;
+    team["battle_gear"] = 0;
+    team["car"] = 0;
+    team["combat_count"] = 0;
+    team["combat_rank_bottom"] = 0;
+    team["combat_rank_top"] = 0;
+    team["dead_rate"] = 0;
+    team["is_valid"] = 0;
+    team["mission_id"] = 0;
+    team["seed"] = 0;
+    team["staff_power"] = 0;
+    team["sub_count"] = 0;
+    team["sub_rank_bottom"] = 0;
+    team["sub_rank_top"] = 0;
+    team["tank"] = 0;
+    team["team_id"] = 0;
+    team["team_power"] = 0;
+    team["time"] = 0;
+    team["truck"] = 0;
+    team["walker_gear"] = 0;
+    team["win_rate"] = 0;
+}
+
 nlohmann::json cmd_get_combat_deploy_list::execute(
     nlohmann::json &data,
     const std::optional<database::players::player> &player) {
@@ -37,7 +62,6 @@ nlohmann::json cmd_get_combat_deploy_list::execute(
         mission["min_win_rate"] = op.get_min_win_rate();
         mission["mission_id"] = op.get_mission_id();
         mission["name_key"] = mission["category"];
-        // printf("%s\n", op.get_primary_reward().c_str());
 
         mission["primary_reward"] =
             nlohmann::json::parse(op.get_primary_reward());
@@ -51,30 +75,39 @@ nlohmann::json cmd_get_combat_deploy_list::execute(
         mission["tank_min"] = op.get_tank_min();
 
         nlohmann::json team;
-        team["armored"] = 0;
-        team["battle_gear"] = 0;
-        team["car"] = 0;
-        team["combat_count"] = 0;
-        team["combat_rank_bottom"] = 0;
-        team["combat_rank_top"] = 0;
-        team["dead_rate"] = 0;
-        team["is_valid"] = 0;
-        team["mission_id"] = 0;
-        team["seed"] = 0;
-        team["staff_power"] = 0;
-        team["sub_count"] = 0;
-        team["sub_rank_bottom"] = 0;
-        team["sub_rank_top"] = 0;
-        team["tank"] = 0;
-        team["team_id"] = 0;
-        team["team_power"] = 0;
-        team["time"] = 0;
-        team["truck"] = 0;
-        team["walker_gear"] = 0;
-        team["win_rate"] = 0;
+        if (op.get_team() == NULL) {
+            create_empty_team(team);
+        } else {
+            auto ops_team_opt = database::ops_team::find(op.get_team());
+            if (ops_team_opt.has_value()) {
+                database::ops_team::ops_team ops_team = ops_team_opt.value();
+                team["armored"] = ops_team.get_armored();
+                team["battle_gear"] = ops_team.get_battle_gear();
+                team["car"] = ops_team.get_car();
+                team["combat_count"] = ops_team.get_combat_count();
+                team["combat_rank_bottom"] = ops_team.get_combat_rank_bottom();
+                team["combat_rank_top"] = ops_team.get_combat_rank_top();
+                team["dead_rate"] = ops_team.get_dead_rate();
+                team["is_valid"] = ops_team.get_is_valid();
+                team["mission_id"] = ops_team.get_mission_id();
+                team["seed"] = ops_team.get_seed();
+                team["staff_power"] = ops_team.get_staff_power();
+                team["sub_count"] = ops_team.get_sub_count();
+                team["sub_rank_bottom"] = ops_team.get_sub_rank_bottom();
+                team["sub_rank_top"] = ops_team.get_sub_rank_top();
+                team["tank"] = ops_team.get_tank();
+                team["team_id"] = ops_team.get_team_id();
+                team["team_power"] = ops_team.get_team_power();
+                team["time"] = ops_team.get_time();
+                team["truck"] = ops_team.get_truck();
+                team["walker_gear"] = ops_team.get_walker_gear();
+                team["win_rate"] = ops_team.get_win_rate();
+            } else {
+                create_empty_team(team);
+            }
+        }
 
         mission["team"] = team;
-        // mission["team"] = op.get_team();
         mission["time"] = op.get_time();
         mission["time_random"] = op.get_time_random();
         mission["truck_max"] = op.get_truck_max();
@@ -82,7 +115,6 @@ nlohmann::json cmd_get_combat_deploy_list::execute(
         mission["walker_gear_max"] = op.get_walker_gear_max();
         mission["walker_gear_min"] = op.get_walker_gear_min();
         mission["win_rate"] = op.get_win_rate();
-        printf("%s\n", mission.dump().c_str());
         mission_list[count++] = mission;
     }
 
